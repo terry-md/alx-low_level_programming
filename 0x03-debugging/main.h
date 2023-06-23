1591134eca913208471da309ab7f30a60ae358b5
terry-md <joshuaaterry01@gmail.com>
2023-06-23 02:41:09 -0700

    2-main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, inr b, int c);
+int largest_number(int a, int b, int c);
 int covert_day(int month, int day);
 
 #endif
